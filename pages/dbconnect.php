d09532371da7b591371ad23aad41e8136061fc86
Pradipsahu17 <46587318+Pradipsahu17@users.noreply.github.com>
2022-07-05 14:29:49 +0545

    Add files via upload

---
+++ pages/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+	$servername = "localhost";
+	$dbusername = "root";
+	$dbpassword = "";
+
+// Create connection
+$conn = mysqli_connect($servername,$dbusername,$dbpassword,"my_website");
+
+ ?>
