fde0b7443c663740c5de602442513073a5cf3ec3
Pradipsahu17 <46587318+Pradipsahu17@users.noreply.github.com>
2022-07-29 15:56:44 +0545

    Delete pages directory

--- pages/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$servername = "localhost";
-	$dbusername = "root";
-	$dbpassword = "";
-
-// Create connection
-$conn = mysqli_connect($servername,$dbusername,$dbpassword,"my_website");
-
- ?>
